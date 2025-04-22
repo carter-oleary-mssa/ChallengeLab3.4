cf4ae9bc46f02617e0ac5d76a251305624726836
carter-oleary <carter.m.oleary@outlook.com>
2025-04-22 16:11:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+string[] arr = { "ABFCACDB", "ACBBD" };
+
+foreach (string str in arr)
+{
+    Console.WriteLine(MinLength(str));
+}
+
+int MinLength(string s)
+{
+    for (int i = 0; i < s.Length - 1; i++)
+    {
+        if (s[i] == 'A' && s[i+1] == 'B')
+        {
+            return MinLength(s.Replace("AB", ""));
+        }
+        if (s[i] == 'C' && s[i + 1] == 'D')
+        {
+            return MinLength(s.Replace("CD", ""));
+        }
+    }
+    return s.Length;
+}
