5bd3b974a322f18a35eec498d4deafcb9fe57a2a
carter-oleary <carter.m.oleary@outlook.com>
2025-04-24 13:36:17 -0400

    Added iterative solution

--- Program.cs
+++ Program.cs
@@ -4,21 +4,32 @@ string[] arr = { "ABFCACDB", "ACBBD" };
 
 foreach (string str in arr)
 {
-    Console.WriteLine(MinLength(str));
+    Console.WriteLine($"The min length of {str} is {MinLengthRec(str)} recursively");
+    Console.WriteLine($"The min length of {str} is {MinLengthIter(str)} iteratively");
 }
 
-int MinLength(string s)
+int MinLengthRec(string s)
 {
     for (int i = 0; i < s.Length - 1; i++)
     {
-        if (s[i] == 'A' && s[i+1] == 'B')
+        if (s[i] == 'A' && s[i + 1] == 'B')
         {
-            return MinLength(s.Replace("AB", ""));
+            return MinLengthRec(s.Replace("AB", ""));
         }
         if (s[i] == 'C' && s[i + 1] == 'D')
         {
-            return MinLength(s.Replace("CD", ""));
+            return MinLengthRec(s.Replace("CD", ""));
         }
+    }
+    return s.Length;
+}
+
+int MinLengthIter(string s)
+{
+    while(s.Contains("AB") || s.Contains("CD"))
+    {
+        s = s.Replace("AB", "");
+        s = s.Replace("CD", "");
     }
     return s.Length;
 }
